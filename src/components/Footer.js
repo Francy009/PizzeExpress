c046f4d9f9a5ad339ada1c1074cf6acff2a0c2db
Francesca Bagordo <bagordofrancesca587@gmail.com>
2024-01-30 23:31:07 +0100

    fix style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,14 @@ import './footer.css'
 
 function Footer() {
     return (
-        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3  border-top footer">
-            <div className="col-md-4 d-flex align-items-center">
+        <footer className="py-3 border-top footer">
+            <div className="container text-center">
                 <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
                     <svg className="bi" width="30" height="24">
                         <use href="#bootstrap"></use>
                     </svg>
                 </a>
-                <span className="text-center p-3">© 2024 Company, Inc</span>
+                <span>© 2024 Company, Inc</span>
             </div>
         </footer>
     )
